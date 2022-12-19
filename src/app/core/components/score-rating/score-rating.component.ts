c43d9c3dcf538e44cf9d8e2f044a41884cc77367
d_derakhshani <d.derakhshani87@gmail.com>
2022-12-20 00:32:23 +0330

    score rating

--- src/app/core/components/score-rating/score-rating.component.ts
+++ src/app/core/components/score-rating/score-rating.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class ScoreRatingComponent implements OnInit {
     @Input()
-    maxScore: number;
+    maxScore: number = 10;
 
     @Input()
     score;
